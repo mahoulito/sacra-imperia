fc99c74d253b34c10cc906b18d695074768088b8
mahoulito <manuripense2004@gmail.com>
2026-05-04 22:28:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,1 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        portal: 'public/portal.html'
-      }
-    }
-  }
-})
+export default {}
